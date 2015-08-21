188bcd95fa9c266dd474c5f5d619ae75f083cb2e
madrus <madrus@gmail.com>
2015-08-21 15:30:07 +0200

    Handling of the AddItem event added to the GroceryListAddItem class

--- app/components/GroceryListAddItem.jsx
+++ app/components/GroceryListAddItem.jsx
@@ -2,12 +2,24 @@
 var React = require('react/addons');
 
 module.exports = React.createClass({
+    getInitialState: function () {
+        return {
+            input: ''
+        };
+    },
+    handleInputName: function (event) { // the target of event is always the input
+        this.setState({input: event.target.value});
+    },
+    addItem: function (event) {
+        event.preventDefault();
+        console.log('Adding item!', this.state.input);
+    },
     render: function () {
         return (
-            <div className='grocery-addItem'>
-                <form>
-                    <input />
-                    <button>Add Item</button>                
+            <div className = 'grocery-addItem'>
+                <form onSubmit={this.addItem}>
+                    <input value={this.state.input} onChange = {this.handleInputName}/>
+                    <button>Add Item< /button>
                 </form>
             </div>
         );
