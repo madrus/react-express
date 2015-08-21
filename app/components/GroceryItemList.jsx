c22277319ba05856d8fb32e52cc7577cfac42044
madrus <madrus@gmail.com>
2015-08-21 12:47:53 +0200

    After module 2

---
+++ app/components/GroceryItemList.jsx
@@ -0,0 +1,11 @@
+var React = require('react/addons');
+
+module.exports = React.createClass({
+    render: function () { // render is a must in any React object
+        return (
+            <div>
+                <h1>Grocery Listify</h1>
+            </div>
+        );
+    }
+});
--- server/main.js
+++ server/main.js
@@ -1,9 +1,10 @@
-'use strict';
+//jshint -W117
 
 var express = require('express');
-var app = module.exports = express();
+var app = express();
 
 app.get('/', function (req, res) {
-	res.render('./../app/index.ejs', {});
+    res.render('./../app/index.ejs', {});
 });
+app.use(express.static(__dirname + '/../.tmp'));
 app.listen(7777);
