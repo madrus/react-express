f0d0fd540409822b716219a8cb90f88f13a0912e
madrus <madrus@gmail.com>
2015-08-21 14:01:32 +0200

    Grocery item rendering is moved from the GroceryItemList to a separate GroceryItem class

--- app/components/GroceryItemList.jsx
+++ app/components/GroceryItemList.jsx
@@ -1,4 +1,6 @@
+//jshint -W117
 var React = require('react/addons');
+var GroceryItem = require('./GroceryItem.jsx');
 
 module.exports = React.createClass({
     render: function () { // render is a must in any React object
@@ -8,7 +10,7 @@ module.exports = React.createClass({
                 <div>
                     {this.props.items.map(function(item, index) {
                         return (
-                        <div>{item.name}</div>
+                            <GroceryItem item={item} key={"item"+index}/>
                         )
                     })
                     }
--- app/main.jsx
+++ app/main.jsx
@@ -1,7 +1,21 @@
+//jshint -W117
+
 var React = require('react/addons');
 
-console.log("Hello from JSX!");
+console.log('Hello from JSX!');
 
 var GroceryItemList = require('./components/GroceryItemList.jsx');
 
-React.render(<GroceryItemList />, app); //1st = what, 2nd = where
+var initial = [{
+    name: 'Ice Cream'
+}, {
+    name: 'Waffles'
+}, {
+    name: 'Candy',
+    purchased: true
+}, {
+    name: 'Snarks'
+}];
+
+/* 1st = what, 2nd = where, items = parameter of  */
+React.render(<GroceryItemList items={initial} />, app);
